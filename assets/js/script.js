99ef40e3d430c2cd94d908ddaaa710e65844f2f6
NycolasSantana <nycolasrobertods16@gmail.com>
2023-03-23 13:21:02 -0300

    base

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+let navg = document.querySelector('#navg');
+function nav(){
+    if(navg.style.display == "none") {
+        navg.style.display = "inline";
+    } else {
+        navg.style.display = "none";
+    }
+    
+};
