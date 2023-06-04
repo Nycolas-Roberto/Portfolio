5c99fc1019e6a69656151144cc19e4f52b54f753
Nycolas-Roberto <nycolasrobertods16@gmail.com>
2023-06-04 15:18:57 -0300

    RESET PORTFÓLIO

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-let navg = document.querySelector('#nav-hamb');
-function nav(){
-    if(navg.style.display == "none") {
-        navg.style.display = "block";
-    } else {
-        navg.style.display = "none";
-    }
-    
-};
